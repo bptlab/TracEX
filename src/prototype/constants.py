eb5ba1a167f4420fa16771d3a48d89e32f7d8181
Nils <nils.schmitt@student.hpi.de>
2023-10-25 19:31:06 +0200

    protect key-info

--- src/prototype/constants.py
+++ src/prototype/constants.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 out_path = Path("content/outputs/") #Path to the outputs-folder
 in_path = Path("content/inputs/") #Path to the inputs-folder
-oaik = "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"
+oaik = "sk-key" #put openAI key here and take it out before pushing to github
 os.environ["OPENAI_API_KEY"] = oaik
 maxtokens=200
 model = "gpt-3.5-turbo"
--- src/prototype/main.py
+++ src/prototype/main.py
@@ -8,5 +8,7 @@ import constants as c
 import inputInquiry as ii
 import inputHandling as ih 
 
+#Put openAI in "constants.py" and take it out of there before pushing to github!
+
 inp = ii.getInput()
 ih.convertInpToCSV(inp)
