46c75d48660e6d4bcb4a4276dacf82391a472706
Sören Schubert <soeren.s95@googlemail.com>
2023-11-26 01:41:52 +0100

    🔍 move pipeline results from ProcessingView to ResultView, add event log and dfg representations

--- src/tracex/extraction/views.py
+++ src/tracex/extraction/views.py
@@ -1,9 +1,18 @@
 import pm4py
+import pandas
+import os
+import tempfile
+import base64
+
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
 from django.core.cache import cache
-import sys
+from io import StringIO, BytesIO
+
+# necessary due to Windows error. see information for your os here:
+# https://stackoverflow.com/questions/35064304/runtimeerror-make-sure-the-graphviz-executables-are-on-your-systems-path-aft
+os.environ["PATH"] += os.pathsep + "C:/Program Files/Graphviz/bin/"
 
 from .forms import JourneyForm
 from .prototype import input_handling
@@ -16,7 +25,7 @@ def index(request):
 class JourneyInputView(generic.FormView):
     form_class = JourneyForm
     template_name = "upload_journey.html"
-    success_url = reverse_lazy("processing")
+    success_url = reverse_lazy("result")
 
     def form_valid(self, form):
         cache.set("journey", form.cleaned_data["journey"])
@@ -30,28 +39,52 @@ class ProcessingView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        journey = cache.get("journey").read().decode("utf-8")
-        event_types = cache.get("event_types")
-        output_path = input_handling.convert_inp_to_xes(journey)
-        with open(output_path) as f:
-            output = f.read()
 
-        # attempt to render dfg
-        # output = pm4py.read_xes(output_path)
-        # output_dfg_file = pm4py.discover_dfg(output, 'concept:Activity', 'date:StartDate', 'caseID')
-        # output_dfg = pm4py.view_dfg(output_dfg_file[0], output_dfg_file[1], output_dfg_file[2])
-
-        context["journey"] = journey
-        context["event_types"] = event_types
-        context["output"] = output
         return context
 
 
 class ResultView(generic.TemplateView):
-    template_name = "result_eventlog.html"
+    template_name = "result.html"
 
     def get_context_data(self, **kwargs):
-        eventlog = self.request.GET.get("event_log", "")
         context = super().get_context_data(**kwargs)
-        context["eventlog"] = cache.get("eventlog")
+
+        journey = cache.get("journey").read().decode("utf-8")
+        event_types = cache.get("event_types")
+        locations = cache.get("locations")
+        output_path = input_handling.convert_inp_to_xes(journey)
+
+        # convert xes dataframe to html and write to buffer
+        output_df = pm4py.read_xes(output_path)
+        xes_html_buffer = StringIO()
+        pandas.DataFrame.to_html(output_df, buf=xes_html_buffer)
+
+        # render dfg from xes dataframe
+        dfg_img_buffer = BytesIO()
+        output_dfg_file = pm4py.discover_dfg(
+            output_df, "concept:Activity", "date:StartDate", "caseID"
+        )
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
+            temp_file_path = temp_file.name
+            pm4py.save_vis_dfg(
+                output_dfg_file[0],
+                output_dfg_file[1],
+                output_dfg_file[2],
+                temp_file_path,
+                rankdir="TB",
+            )
+
+        with open(temp_file_path, "rb") as temp_file:
+            dfg_img_buffer.write(temp_file.read())
+
+        os.remove(temp_file_path)
+
+        dfg_img_base64 = base64.b64encode(dfg_img_buffer.getvalue()).decode("utf-8")
+
+        context["event_types"] = event_types
+        context["locations"] = locations
+        context["journey"] = journey
+        context["dfg_img"] = dfg_img_base64
+        context["xes_html"] = xes_html_buffer.getvalue()
         return context
