b00142a8ebbe11b30c8c1172f493397354af5961
Nils <nils.schmitt@student.hpi.de>
2023-11-27 12:13:52 +0100

    add function docstring to main

--- src/prototype/main.py
+++ src/prototype/main.py
@@ -5,6 +5,7 @@ import output_handling as oh
 
 
 def main():
+    """Main function calling every pipeline step needed to run the program."""
     ii.greeting()
     input_text = ii.get_input()
     ih.convert_text_to_csv(input_text)
