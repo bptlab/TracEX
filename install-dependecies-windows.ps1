b6c577a5ae854a952ba429356aed26724666d70d
Nils Schmitt <nils.schmitt@student.hpi.de>
2023-12-22 12:48:22 +0100

    📚 extend README by installation guide

--- install-dependecies-windows.ps1
+++
@@ -1,6 +0,0 @@
-# install packages needed for using TracEX
-winget install Python --accept-source-agreements --accept-package-agreements
-winget install graphviz --accept-source-agreements --accept-package-agreements
-
-# install python packages
-pip install -r .\requirements.txt
