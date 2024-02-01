749cfeb562704132775e5e2d44e9fb568172f834
Sören Schubert <soeren.s95@googlemail.com>
2024-02-01 15:55:26 +0100

    🐛 fix bug where start and end dates were extracted in the wrong format

--- tracex/extraction/logic/modules/module_time_extractor_backup.py
+++ tracex/extraction/logic/modules/module_time_extractor_backup.py
@@ -25,7 +25,9 @@ class TimeExtractorBackup(Module):
         super().execute(df, patient_journey)
         df["start"] = df["event_information"].apply(self.__extract_start_date)
         df["end"] = df.apply(self.__extract_end_date, axis=1)
+        print(df, "\n\n")
         df = self.__post_processing(df)
+        print(df)
         df["duration"] = df.apply(self.__calculate_duration, axis=1)
         self.result = df
 
@@ -61,7 +63,7 @@ class TimeExtractorBackup(Module):
         """Calculate the duration of an activity."""
         start_date = datetime.strptime(row["start"], "%Y%m%dT%H%M")
         end_date = datetime.strptime(row["end"], "%Y%m%dT%H%M")
-        duration = start_date - end_date
+        duration = end_date - start_date
         hours, remainder = divmod(duration.total_seconds(), 3600)
         minutes, seconds = divmod(remainder, 60)
 
@@ -76,16 +78,30 @@ class TimeExtractorBackup(Module):
 
             return row
 
-        try:
-            pd.to_datetime(df["start"], format="%Y%m%dT%H%M", errors="raise")
-        except ValueError:
-            df["start"] = pd.NaT
+        # try:
+        #     pd.to_datetime(df["start"], format="%Y%m%dT%H%M", errors="raise")
+        # except ValueError:
+        #     df["start"] = pd.NaT
+        # df["start"].ffill()
+        #
+        # try:
+        #     pd.to_datetime(df["end"], format="%Y%m%dT%H%M", errors="raise")
+        # except ValueError:
+        #     df["end"] = pd.NaT
+        # df["end"].ffill()
+        #
+        # df = df.apply(fix_end_dates, axis=1)
+
+        converted_start = pd.to_datetime(
+            df["start"], format="%Y%m%dT%H%M", errors="coerce"
+        )
+        mask = converted_start.isna()
+        df.loc[mask, "start"] = converted_start
         df["start"].ffill()
 
-        try:
-            pd.to_datetime(df["end"], format="%Y%m%dT%H%M", errors="raise")
-        except ValueError:
-            df["end"] = pd.NaT
+        converted_end = pd.to_datetime(df["end"], format="%Y%m%dT%H%M", errors="coerce")
+        mask = converted_end.isna()
+        df.loc[mask, "end"] = converted_end
         df["end"].ffill()
 
         df = df.apply(fix_end_dates, axis=1)
@@ -104,30 +120,43 @@ class TimeExtractorBackup(Module):
 
 
 START_DATE_CONTEXT = """
-    You are provided with a natural language text containing various events. Your task is to identify the start date of
-    a specific activity mentioned in the text. The activity label will be provided, and it is your job to extract only
-    the start date associated with this activity from the text.
-    Under no circumstances put anything else in the ouptut apart from the extracted start date.
-    Please follow the following rules:
-    1. The format of the date should always be YYYYMMDDT0000. For example, 20200101T0000.
-    2. If only a month is mentioned then the date should always be the first day of the month. For example for March it should be 20200301T0000.
-    3. If the date is mentioned in a different format, please convert it to the format mentioned above.
-    4. Translate formulations like "the next day" or "over the following weeks" to the corresponding date.
-    5. Also consider context information from previous activities and their start dates.
-    6. If the start date is not mentioned output "N/A" instead.
+    Extract the start date in the format YYYYMMDDT0000 for the corresponding activity label from the patient journey. It is MANDATORY that you do the following:
+    You may not return anything but a date in the format YYYYMMDDT0000. For example, 20200101T0000. Do not return anything else.
+    You are forbidden to return any other information or sentence. If you have any doubt return "20200101T0000", but without the "" symbols.
 """
 
+
+# """
+#     You are provided with a natural language text containing various events. Your task is to identify the start date of
+#     a specific activity mentioned in the text. The activity label will be provided, and it is your job to extract only
+#     the start date associated with this activity from the text.
+#     Under no circumstances put anything else in the ouptut apart from the extracted start date.
+#     Please follow the following rules:
+#     1. The format of the date should always be YYYYMMDDT0000. For example, 20200101T0000.
+#     2. If only a month is mentioned then the date should always be the first day of the month. For example for March it should be 20200301T0000.
+#     3. If the date is mentioned in a different format, please convert it to the format mentioned above.
+#     4. Translate formulations like "the next day" or "over the following weeks" to the corresponding date.
+#     5. Also consider context information from previous activities and their start dates.
+#     6. If the start date is not mentioned output "N/A" instead.
+# """
+
 END_DATE_CONTEXT = """
-    You are provided with a natural language text containing various events. Your task is to identify the end date of
-    a specific activity mentioned in the text. The activity label and the corresponding start date will be provided,
-    and it is your job to extract only the end date associated with this activity from the text.
-    Under no circumstances put anything else in the ouptut apart from the extracted end date.
-    Please follow the following rules:
-    1. The format of the date should always be YYYYMMDDT0000. For example, 20200101T0000.
-    2. If only a month in mentioned then the date should always be the first day of the month. For example for March it should be 20200301T0000.
-    3. If the date is mentioned in a different format, please convert it to the format mentioned above.
-    4. Translate formulations like "the next day" or "over the following weeks" to the corresponding date.
-    5. Also consider context information from previous activities and their start dates and end dates.
-    6. End dates can not be earlier than the start dates.
-    7. If the end date is not mentioned output "N/A" instead.
+    Extract the end date in the format YYYYMMDDT0000 for the corresponding activity label from the patient journey. It is MANDATORY that you do the following:
+    You may not return anything but a date in the format YYYYMMDDT0000. For example, 20200101T0000. Do not return anything else.
+    You are forbidden to return any other information or sentence. If you have any doubt return "20200101T0000", but without the "" symbols.
 """
+
+# """
+#     You are provided with a natural language text containing various events. Your task is to identify the end date of
+#     a specific activity mentioned in the text. The activity label and the corresponding start date will be provided,
+#     and it is your job to extract only the end date associated with this activity from the text.
+#     Under no circumstances put anything else in the ouptut apart from the extracted end date.
+#     Please follow the following rules:
+#     1. The format of the date should always be YYYYMMDDT0000. For example, 20200101T0000.
+#     2. If only a month in mentioned then the date should always be the first day of the month. For example for March it should be 20200301T0000.
+#     3. If the date is mentioned in a different format, please convert it to the format mentioned above.
+#     4. Translate formulations like "the next day" or "over the following weeks" to the corresponding date.
+#     5. Also consider context information from previous activities and their start dates and end dates.
+#     6. End dates can not be earlier than the start dates.
+#     7. If the end date is not mentioned output "N/A" instead.
+# """
