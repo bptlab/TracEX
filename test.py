80ceef3efbf89ad3bfbc5385356071e7af770c0e
Kien <tkv29@yahoo.de>
2024-02-04 18:50:27 +0100

    ♻️ refactor measure function for event types and location classification

--- test.py
+++ test.py
@@ -15,9 +15,9 @@ text = open(u.input_path / "journey_synth_covid_0.txt").read()
 # df = ih.convert_text_to_bulletpoints(text)
 # print(df)
 
-print(m.measure_event_information(text))
+# print(m.measure_event_information(text))
 # print(m.measure_event_types(text))
-# print(m.measure_location(text))
+print(m.measure_location(text))
 
 
 # df = m.measure_event_types(text)
--- tracex/extraction/prototype/metrics.py
+++ tracex/extraction/prototype/metrics.py
@@ -17,77 +17,6 @@ def measure_event_information(text):
     return df
 
 
-def measure_event_types(text):
-    df = ih.convert_text_to_bulletpoints(text)
-    new_df = pd.DataFrame(
-        [], columns=["event_type", "(token1, lin_prob1)", "(token2, lin_prob2)"]
-    )
-    values_list = df.values.tolist()
-    for item in values_list:
-        messages = [
-            {"role": "system", "content": p.EVENT_TYPE_CONTEXT},
-            {
-                "role": "user",
-                "content": p.EVENT_TYPE_PROMPT + "\nThe bulletpoint: " + item[0],
-            },
-            {"role": "assistant", "content": p.EVENT_TYPE_ANSWER},
-        ]
-        content, top_logprops = u.query_gpt(messages, logprobs=True, top_logprobs=2)
-        metrics = [content]
-
-        for logprob in top_logprops:
-            token = logprob.token
-            lin_prop = calculate_linear_probability(logprob.logprob)
-            metrics.append((token, lin_prop))
-
-        new_row = pd.DataFrame(
-            [metrics],
-            columns=["event_type", "(token1, lin_prob1)", "(token2, lin_prob2)"],
-        )
-        new_df = pd.concat([new_df, new_row], ignore_index=True)
-        ih.document_intermediates(new_row.to_string())
-        print(new_row.to_string())
-    df = pd.concat([df, new_df], axis=1)
-    return df
-
-
-def measure_location(text):
-    df = ih.add_event_types(ih.convert_text_to_bulletpoints(text))
-    new_df = pd.DataFrame(
-        [], columns=["location", "(token1, lin_prob1)", "(token2, lin_prob2)"]
-    )
-    values_list = df.values.tolist()
-    event_type_key = df.columns.get_loc("event_type")
-    for item in values_list:
-        messages = [
-            {"role": "system", "content": p.LOCATION_CONTEXT},
-            {
-                "role": "user",
-                "content": p.LOCATION_PROMPT
-                + item[0]
-                + "\nThe category: "
-                + item[event_type_key],
-            },
-            {"role": "assistant", "content": p.LOCATION_ANSWER},
-        ]
-        content, top_logprops = u.query_gpt(messages, logprobs=True, top_logprobs=2)
-        metrics = [content]
-
-        for logprob in top_logprops:
-            token = logprob.token
-            lin_prop = calculate_linear_probability(logprob.logprob)
-            metrics.append((token, lin_prop))
-
-        new_row = pd.DataFrame(
-            [metrics],
-            columns=["location", "(token1, lin_prob1)", "(token2, lin_prob2)"],
-        )
-        new_df = pd.concat([new_df, new_row], ignore_index=True)
-        ih.document_intermediates(new_row.to_string())
-    df = pd.concat([df, new_df], axis=1)
-    return df
-
-
 def rate_event_information(event_information, text):
     category_mapping = {
         "Not Relevant": 1,
@@ -115,6 +44,68 @@ def rate_event_information(event_information, text):
     weighted_relevance = relevance * lin_prop
 
     return (weighted_relevance, category, lin_prop)
+
+
+def measure_event_types(text):
+    df = ih.convert_text_to_bulletpoints(text)
+
+    df[["event_type", "(token_1, lin_prob_1)", "(token_2, lin_prob_2)"]] = df[
+        "event_information"
+    ].apply(lambda event_information: pd.Series(rate_event_type(event_information)))
+
+    return df
+
+
+def rate_event_type(event_information):
+    messages = [
+        {"role": "system", "content": p.EVENT_TYPE_CONTEXT},
+        {
+            "role": "user",
+            "content": p.EVENT_TYPE_PROMPT + "\nThe bulletpoint: " + event_information,
+        },
+        {"role": "assistant", "content": p.EVENT_TYPE_ANSWER},
+    ]
+
+    event_type, top_logprops = u.query_gpt(messages, logprobs=True, top_logprobs=2)
+    token1 = top_logprops[0].token
+    lin_prob1 = calculate_linear_probability(top_logprops[0].logprob)
+    token_2 = top_logprops[1].token
+    lin_prob2 = calculate_linear_probability(top_logprops[1].logprob)
+
+    return (event_type, (token1, lin_prob1), (token_2, lin_prob2))
+
+
+def measure_location(text):
+    df = ih.add_event_types(ih.convert_text_to_bulletpoints(text))
+    df[["location", "(token_1, lin_prob_1)", "(token_2, lin_prob_2)"]] = df.apply(
+        lambda row: pd.Series(
+            rate_location(row["event_information"], row["event_type"])
+        ),
+        axis=1,
+    )
+
+    return df
+
+
+def rate_location(event_information, event_type):
+    messages = [
+        {"role": "system", "content": p.LOCATION_CONTEXT},
+        {
+            "role": "user",
+            "content": p.LOCATION_PROMPT
+            + event_information
+            + "\nThe category: "
+            + event_type,
+        },
+        {"role": "assistant", "content": p.LOCATION_ANSWER},
+    ]
+    location, top_logprops = u.query_gpt(messages, logprobs=True, top_logprobs=2)
+    token_1 = top_logprops[0].token
+    lin_prob_1 = calculate_linear_probability(top_logprops[0].logprob)
+    token_2 = top_logprops[1].token
+    lin_prob_2 = calculate_linear_probability(top_logprops[1].logprob)
+
+    return (location, (token_1, lin_prob_1), (token_2, lin_prob_2))
 
 
 def calculate_linear_probability(logprob):
