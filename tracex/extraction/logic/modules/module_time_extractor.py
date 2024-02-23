883f12a85559ffbacbb7136f2dff8697e48559a4
Kien <tkv29@yahoo.de>
2024-02-23 22:51:59 +0100

    change start / end to start_date / end_date

--- tracex/extraction/logic/modules/module_time_extractor.py
+++ tracex/extraction/logic/modules/module_time_extractor.py
@@ -22,8 +22,8 @@ class TimeExtractor(Module):
     @log_execution_time(Path("extraction/logs/execution_time.log"))
     def execute(self, df, patient_journey=None):
         super().execute(df, patient_journey)
-        df["start"] = df["event_information"].apply(self.__extract_start_date)
-        df["end"] = df.apply(self.__extract_end_date, axis=1)
+        df["start_date"] = df["event_information"].apply(self.__extract_start_date)
+        df["end_date"] = df.apply(self.__extract_end_date, axis=1)
         df["duration"] = df.apply(self.__calculate_row_duration, axis=1)
         return df
 
@@ -57,7 +57,7 @@ class TimeExtractor(Module):
             {
                 "role": "user",
                 "content": f"{p.END_DATE_PROMPT} \nThe text: {self.patient_journey} \nThe bulletpoint: "
-                f"{row['event_information']} \nThe start date: {row['start']}",
+                f"{row['event_information']} \nThe start date: {row['start_date']}",
             },
             {"role": "assistant", "content": p.END_DATE_ANSWER},
         ]
@@ -76,10 +76,10 @@ class TimeExtractor(Module):
     @staticmethod
     def __calculate_row_duration(row):
         """Calculate the duration for a given activity."""
-        if row["start"] == "N/A" or row["end"] == "N/A":
+        if row["start_date"] == "N/A" or row["end_date"] == "N/A":
             return "N/A"
-        start_date = datetime.strptime(row["start"], "%Y%m%dT%H%M")
-        end_date = datetime.strptime(row["end"], "%Y%m%dT%H%M")
+        start_date = datetime.strptime(row["start_date"], "%Y%m%dT%H%M")
+        end_date = datetime.strptime(row["end_date"], "%Y%m%dT%H%M")
         duration = end_date - start_date
         hours, remainder = divmod(duration.total_seconds(), 3600)
         minutes, seconds = divmod(remainder, 60)
