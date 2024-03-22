fca72ef8b6b09d07fdbd21dd6659022d6d3e1127
tkv29@yahoo.de <tkv29@yahoo.de>
2024-03-22 23:58:13 +0100

    added doc strings

--- tracex/patient_journey_generator/apps.py
+++ tracex/patient_journey_generator/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 
 class PatientJourneyGeneratorConfig(AppConfig):
     """App configuration class for django UI."""
-    
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "patient_journey_generator"
