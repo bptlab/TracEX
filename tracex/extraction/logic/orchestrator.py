7c2a2f3e984dacc37b1cdc8de03cc9bfb04eebca
tkv29@yahoo.de <tkv29@yahoo.de>
2024-03-25 12:09:10 +0100

    add one global style.css

--- tracex/extraction/logic/orchestrator.py
+++ tracex/extraction/logic/orchestrator.py
@@ -125,9 +125,6 @@ class Orchestrator:
             self.data.insert(0, "case_id", latest_id + 1)
             self.data.to_csv(utils.CSV_OUTPUT, index=False, header=True)
 
-    # This method may be deleted later. The original idea was to always call Orchestrator.run() and depending on if
-    # a configuration was given or not, the patient journey generation may be executed.
-
     def save_results_to_db(self):
         """Save the trace to the database."""
         patient_journey: PatientJourney = PatientJourney.manager.get(
