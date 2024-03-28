3f3718eb7701aecd998ffd3f8b568b0b02b00ca6
tkv29@yahoo.de <tkv29@yahoo.de>
2024-03-29 00:56:48 +0100

    fixed spinning loader

--- tracex/patient_journey_generator/static/patient_journey_generator/js/loading-spinner.js
+++ tracex/patient_journey_generator/static/patient_journey_generator/js/loading-spinner.js
@@ -2,4 +2,5 @@ const generate_button = document.getElementById('generate_button');
 const loader = document.getElementById('loading-spinner');
 
 generate_button.addEventListener('click', () => {
+    loader.classList.remove('not_visible');
 })
