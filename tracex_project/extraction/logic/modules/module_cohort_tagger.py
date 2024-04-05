6e3513454415ea771ff0099ffa4731a19b65f9d8
Pit Buttchereit <116184086+PitButtchereit@users.noreply.github.com>
2024-04-05 12:53:24 +0200

    🚨 add tests.py

--- tracex_project/extraction/logic/modules/module_cohort_tagger.py
+++ tracex_project/extraction/logic/modules/module_cohort_tagger.py
@@ -21,6 +21,7 @@ class CohortTagger(Module):
 
     @log_execution_time(Path(settings.BASE_DIR / "tracex/logs/execution_time.log"))
     def execute_and_save(self, df, patient_journey=None):
+        """Executes the module and saves the results in the database."""
         super().execute(df, patient_journey)
 
         return self.__extract_cohort_tags()
