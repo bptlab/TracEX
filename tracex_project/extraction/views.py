914939e45d65664fe50d249f84e6cbc78739165a
Van Thang Nguyen <anyzyo@gmail.com>
2024-04-19 15:01:02 +0200

    refactor: methods are now static in downloadxesclass

--- tracex_project/extraction/views.py
+++ tracex_project/extraction/views.py
@@ -18,10 +18,6 @@ from .forms import JourneyForm, ResultForm, FilterForm
 from .logic.orchestrator import Orchestrator, ExtractionConfiguration
 from .models import Trace
 
-
-
-
-
 # necessary due to Windows error. see information for your os here:
 # https://stackoverflow.com/questions/35064304/runtimeerror-make-sure-the-graphviz-executables-are-on-your-systems-path-aft
 os.environ["PATH"] += os.pathsep + "C:/Program Files/Graphviz/bin/"
@@ -279,6 +275,7 @@ class DownloadXesView(View):
 
         return self.prepare_response(files_to_download)
 
+    @staticmethod
     def get_trace_types(self, request):
         """Retrieves a list of trace types from the POST data."""
 
@@ -297,6 +294,7 @@ class DownloadXesView(View):
 
         return files_to_download
 
+    @staticmethod
     def process_trace_type(self, request, trace_type):
         """Process and provide the XES files to be downloaded based on the trace type."""
         if trace_type == 'all_traces':
@@ -313,6 +311,7 @@ class DownloadXesView(View):
 
         return self.zip_files_response(files_to_download)
 
+    @staticmethod
     # pylint: disable=consider-using-with
     def single_file_response(self, file_path):
         """Prepares a file if there is only a single XES file."""
@@ -322,6 +321,7 @@ class DownloadXesView(View):
 
         return response
 
+    @staticmethod
     def zip_files_response(self, files_to_download):
         """Prepares a zip file if there are multiple XES files using a temporary file."""
         temp_zip = NamedTemporaryFile(mode='w+b', suffix='.zip', delete=False)
