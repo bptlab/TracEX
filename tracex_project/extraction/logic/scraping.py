97dbb514a58b9755fd087b0fc3738b13bcd3ae45
Nils Schmitt <nils.schmitt@student.hpi.de>
2024-04-23 13:29:55 +0200

    :sparkles: improve scraping PoC

--- tracex_project/extraction/logic/scraping.py
+++ tracex_project/extraction/logic/scraping.py
@@ -16,34 +16,35 @@ driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 # Navigate to the reddit page
 driver.get(
-    "https://www.reddit.com/r/COVID19positive/comments/1c9mh4h/severe_periods_every_23_months_since_first/"
+    "https://www.reddit.com/r/COVID19positive/comments/1caz5sn/my_second_positive_has_been_a_nightmare/"
 )
 
 # Find an click the "read more" button if there is one
-read_more_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.ID, "t3_1c9mh4h-read-more-button"))
-)
-if read_more_button:
+try:
+    # As this button is found by id, it's only working for this and short enough posts.
+    # Finding the button by calss isn't possible, due to the naming...
+    read_more_button = WebDriverWait(driver, 2).until(
+        EC.element_to_be_clickable((By.ID, "t3_1caz5sn-read-more-button"))
+    )
     read_more_button.click()
-
-# Wait for the button click action to complete
-WebDriverWait(driver, 10).until_not(
-    EC.presence_of_element_located((By.ID, "t3_1c9mh4h-read-more-button"))
-)
+except:
+    None
 
 # Scrape the top posts
-content = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, ".mb-sm.mb-xs.px-md.xs\:px-0"))
+title = driver.find_element(
+    By.CSS_SELECTOR,
+    ".font-semibold.text-neutral-content-strong.m-0.text-18.xs\:text-24.mb-xs.px-md.xs\:px-0.xs\:mb-md",
 )
+content = driver.find_element(By.CSS_SELECTOR, ".mb-sm.mb-xs.px-md.xs\:px-0")
 with open(input_path / "scraped_patient_journey.txt", "w") as f:
-    f.write(content.text)
+    f.write(f"{title.text}\n\n{content.text}")
 
 # Scrape the comments
 comments = driver.find_elements(
     By.CSS_SELECTOR, ".py-0.xs\:mx-xs.mx-2xs.inline-block.max-w-full"
 )
 for comment in comments:
-    print(comment.text)
+    print(f"-----\n{comment.text}")
 
 # Quit the driver
 driver.quit()
