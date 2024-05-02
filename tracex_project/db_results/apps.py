adf72b7d59ba3307095676425dbd4a44803cba64
Kien <tkv29@yahoo.de>
2024-05-02 20:02:45 +0200

    remove trailing whitespaces

--- tracex_project/db_results/apps.py
+++ tracex_project/db_results/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 
 class DbResultsConfig(AppConfig):
     """App configuration class for django UI."""
-    
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'db_results'
--- tracex_project/extraction/apps.py
+++ tracex_project/extraction/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 
 class ExtractionConfig(AppConfig):
     """App configuration class for django UI."""
-    
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "extraction"
