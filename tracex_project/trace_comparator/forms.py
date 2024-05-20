0b3a512cb19fd65cee3d63d7555f238ae03c4d41
FR-SON <rupprecht.f@icloud.com>
2024-05-20 17:26:41 +0200

    ♻️update docstrings in trace_comparator/forms.py

--- tracex_project/trace_comparator/forms.py
+++ tracex_project/trace_comparator/forms.py
@@ -4,19 +4,21 @@ from extraction.models import PatientJourney
 
 
 class PatientJourneySelectForm(forms.Form):
-    """Form for selecting ground truth patient journey."""
+    """Form for selecting a patient journey to use in the trace testing environment."""
 
     selected_patient_journey = forms.ChoiceField(choices=[])
 
     def __init__(self, *args, **kwargs):
-        """Initializes the PatientJourneySelectForm."""
+        """Initializes the PatientJourneySelectForm with available choices."""
         super().__init__(*args, **kwargs)
         self.fields[
             "selected_patient_journey"
         ].choices = self.get_patient_journey_choices()
 
-    def get_patient_journey_choices(self):
-        """Retrieves the available patient journey choices from the database."""
+    @staticmethod
+    def get_patient_journey_choices():
+        """Retrieves the available patient journey choices from the database. Available choices are those with a
+        saved ground truth."""
         patient_journeys = PatientJourney.manager.filter(
             name__contains="journey_comparison_"
         )
