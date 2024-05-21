fe7b7b54a98a0efd084df67149e15483e810e4a7
Pit Buttchereit <116184086+PitButtchereit@users.noreply.github.com>
2024-05-21 17:24:46 +0200

    💎 fix relative import

--- tracex_project/tracex/urls.py
+++ tracex_project/tracex/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from tracex import views
 
 urlpatterns = [
     path("", include("patient_journey_generator.urls")),
