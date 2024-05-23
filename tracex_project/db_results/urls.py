1e91fad1b25ac414116047a474533e081786f4d6
Van Thang Nguyen <anyzyo@gmail.com>
2024-05-23 04:01:23 +0200

    refactor: format coding style in urls.py

--- tracex_project/db_results/urls.py
+++ tracex_project/db_results/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path("db_results/download-xes/", views.DownloadXesEvaluationView.as_view(), name="download_xes_evaluation"),
+    path("db_results/download-xes/",
+         views.DownloadXesEvaluationView.as_view(),
+         name="download_xes_evaluation"),
     path(
         "db_results/",
         views.DbResultsOverviewView.as_view(),
